bd7d38fc323ef274ab407413a07e3b43af061dd8
joker <alexkeman9@gmail.com>
2020-10-10 11:06:07 +0800

    new

--- Controller/management.js
+++ Controller/management.js
@@ -1,5 +1,6 @@
 const sequelize = require('../config/sequelize')
 const requestModel = require('../Models/request')
+const bookingModel = require('../Models/booking')
 
 exports.get_booking_guides = async (req, res, next) => {
     const q = req.body.action === "sender_id" ? "receiver_id" : "sender_id"
@@ -20,18 +21,18 @@ exports.get_booking_guides = async (req, res, next) => {
 }
 
 exports.get_booking_agency = async (req, res, next) => {
-    let sender_data = await sequelize.query(`
+    let booking_data = await sequelize.query(`
         select b.*, 
-        taa.id as ads_id, taa.title as ads_title,  taa.image as ads_image,
-        a.id as agency_id, a.username as agency_username, a.rating as agency_rating
+        taa.id as ads_id, taa.title as ads_title, taa.image as ads_image, taa.cost as ads_price, taa.start_date as ads_start_date,
+        u.id as user_id, u.username as user_username, u.email as user_email, u.phone as user_phone
         from bookings b
         join tours_agency_ads taa on b.tours_id=taa.id
-        join agencies a on b.receiver_id = a.id
-        where b.${req.body.action}=${req.userId} and receiver_type='A'
+        join users u on b.receiver_id = u.id
+        where b.receiver_id=${req.userId} and receiver_type='A'
     `)
 
     return res.status(200).json({
-        data: sender_data[0],
+        data: booking_data[0],
         err: null
     })
 }
@@ -56,7 +57,7 @@ exports.get_booking_user = async (req, res, next) => {
 }
 
 exports.update_booking_user = async (req, res, next) => {
-    const req_data = await requestModel.findOne({
+    const req_data = await bookingModel.findOne({
         where: {
             id: req.body.request_id
         }
@@ -69,10 +70,37 @@ exports.update_booking_user = async (req, res, next) => {
     }
 
     if (req.body.action === 'update') {
-        req_data.isPaying = 1
+        req_data.is_payed = 1
         await req_data.save()
     }
-    else { 
+    else {
+        await req_data.destroy();
+    }
+
+    return res.status(200).json({
+        msg: "Success",
+        err: null
+    })
+}
+
+exports.update_booking_agency = async (req, res, next) => {
+    const req_data = await bookingModel.findOne({
+        where: {
+            id: req.body.request_id
+        }
+    })    
+
+    if (!req_data) {
+        return res.status(200).json({
+            err: "Can't find Bookings data!"
+        })
+    }
+
+    if (req.body.action === 'update') {
+        req_data.is_active = 1
+        await req_data.save()
+    }
+    else {
         await req_data.destroy();
     }
 
--- Routes/management.js
+++ Routes/management.js
@@ -8,7 +8,7 @@ router.post('/agency', jwt_validation, managementController.get_booking_agency)
 router.post('/user', jwt_validation, managementController.get_booking_user)
 
 router.post('/guides/update', jwt_validation, managementController.update_booking)
-router.post('/agency/update', jwt_validation, managementController.update_booking)
+router.post('/agency/update', jwt_validation, managementController.update_booking_agency)
 router.post('/user/update', jwt_validation, managementController.update_booking_user)
 
 
